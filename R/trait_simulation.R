563aa385207327706ab2ead863a66daf7e9cf40a
gaoyingnan-academic <52718100+gaoyingnan-academic@users.noreply.github.com>
2019-07-19 09:52:52 -0400

    Update trait_simulation.R

--- R/trait_simulation.R
+++ R/trait_simulation.R
@@ -15,3 +15,31 @@ simulateBMTrait = function(phy,start=0){
   return(trait.sim)
 }
 #
+simulateRateInPhylogeny = function(phy,model,rate,probs){
+  res.phy = phy
+  if(is.null(model)){
+    multipliers = sample(x = rate,size = length(phy$edge.length),replace = TRUE,prob = probs)
+  }else{
+    func = match.fun(paste0("r",model$distr))
+    multipliers = do.call(func,c(list(n=length(phy$edge.length)),model$fullparams[model$params]))
+  }
+  if(model$homogenous.sisters){
+    for(n in Ntip(phy)+1:Nnode(phy)){
+      res.phy$edge.length[res.phy$edge[,1]==n] = phy$edge.length[phy$edge[,1]==n]*multipliers[n-Ntip(phy)]*
+        do.call(model$scalefunc,c(list(l=sum(phy$edge.length[phy$edge[,1]==n])),model$fullparams))
+    }
+  }else{
+    res.phy$edge.length = phy$edge.length*multipliers*do.call(model$scalefunc,c(list(l=phy$edge.length),model$fullparams))
+  }
+  return(res.phy)
+}
+#
+simulateTrait = function(phy,start=0,BM.tree = FALSE,...){
+  res.phy = simulateRateInPhylogeny(phy,...)
+  trait = simulateBMTrait(res.phy,start)
+  if(BM.tree){
+    return(list(trait=trait,BM.phy=BM.phy))
+  }else{
+    return(trait)
+  }
+}
