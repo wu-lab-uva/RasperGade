d2a1415664f358eb11080e271f1f41145b7a1e2a
gaoyingnan-academic <52718100+gaoyingnan-academic@users.noreply.github.com>
2021-03-25 14:17:11 -0400

    Update FigS2.R

--- Preprint2021/FigS2.R
+++ Preprint2021/FigS2.R
@@ -1,27 +1,33 @@
-#
-setwd("F:/Dropbox/Scripts/16S_Copy_Number/Version3/")
-source("fitPE_functions.R")
-setwd("F:/Dropbox/Scripts/16S_Copy_Number/Version4/")
-source("RasperGade_reconstruction.R")
-source("RasperGade_diagnosis.R")
-setwd("F:/Dropbox/Scripts/16S_Copy_Number/20210302/")
+# load required packages and functions
+source("../R/ape_addition.R")
+source("../R/PEpois.R")
+source("../R/fitPE_functions.R")
+source("../R/RasperGade_reconstruction.R")
+source("../R/RasperGade_diagnosis.R")
 library(ggplot2)
 library(ggpubr)
-# parse simulation results
+
+# set the variation in each parameters
 epsilons=c(0,1,10)
 contributions = rev(c(0,0.1,0.3,0.5,0.7,0.9,1))
 replicates = 1:100
 batch = 1
-pars.epsilon = expand.grid(epsilon=epsilons,lambda=5,contribution=0.9,rep=replicates,batch=batch,KEEP.OUT.ATTRS = FALSE)
+
+# recreate the experimental design of the simulations (the second scheme)
+pars.epsilon = expand.grid(epsilon=epsilons,lambda=5,contribution=0,rep=replicates,batch=batch,KEEP.OUT.ATTRS = FALSE)
 pars.epsilon$id = rep(1:(dim(pars.epsilon)[1]/length(unique(pars.epsilon$batch))),length(unique(pars.epsilon$batch)))
-pars.contribution = expand.grid(epsilon=10,lambda=5,contribution=contributions,rep=replicates,batch=batch,KEEP.OUT.ATTRS = FALSE)
+pars.contribution = expand.grid(epsilon=0,lambda=5,contribution=contributions,rep=replicates,batch=batch,KEEP.OUT.ATTRS = FALSE)
 pars.contribution$id = rep(1:(dim(pars.contribution)[1]/length(unique(pars.contribution$batch))),length(unique(pars.contribution$batch)))
 
 # read in expected statistics
-stat.expectation = readRDS("stat.expectation.RDS")
-# epsilon results
+stat.expectation = readRDS("Result/stat.expectation.RDS")
+
+# parse results of simulation with time-independent variation
+epsilon.path = "Result/simulation_Epsilon/"
+epsilon.name = "simulation.epsilon"
+## hidden states
 epsilon.df = do.call(rbind,lapply(1:dim(pars.epsilon)[1],function(i){
-  asr.res = readRDS(paste0("simulation_Epsilon_only/","/bac.simulation.simulation.epsilon.only.",pars.epsilon$id[i],".RDS"))
+  asr.res = readRDS(paste0(epsilon.path,epsilon.name,pars.epsilon$id[i],".RDS"))
   this.stats = sapply(asr.res,function(y){
     if(is.data.frame(y$CV)){
       ers = pseudo.Z.score(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$x,
@@ -45,59 +51,9 @@ epsilon.df = do.call(rbind,lapply(1:dim(pars.epsilon)[1],function(i){
   })
   return(data.frame(rss=this.stats[1,],h=this.stats[2,],cp95=this.stats[3,],D=this.stats[4,],model=c("BM","True model")))
 }))
+## ancestral states
 epsilon.ace.df = do.call(rbind,lapply(1:dim(pars.epsilon)[1],function(i){
-  asr.res = readRDS(paste0("simulation_Epsilon_only","/bac.simulation.simulation.epsilon.only.",pars.epsilon$id[i],".RDS"))
-  this.stats = sapply(asr.res,function(y){
-    if(is.data.frame(y$ASR)){
-      ers = pseudo.Z.score(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$x,
-                           error = y$ASR$var,epsilon = y$FMR$params$epsilon,laplace = FALSE)
-      c(analyzeResidualErrorByCI(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$x,
-                                 error = y$ASR$var,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20),
-        analyzeResidualErrorByHeteroscedasticity(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$x,
-                                                 error = y$ASR$var,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20)[1],
-        1-sum(sapply(ers$p,function(x){min(x,1-x)<=0.025}))/length(ers$p),
-        ks.test(ers$q,pnorm)$statistic)
-    }else{
-      ers = pseudo.Z.score(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$ace$x,
-                           error = y$ASR$error,epsilon = y$FMR$params$epsilon,laplace = FALSE)
-      c(analyzeResidualErrorByCI(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$ace$x,
-                                 error = y$ASR$error,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20),
-        analyzeResidualErrorByHeteroscedasticity(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$ace$x,
-                                                 error = y$ASR$error,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20)[1],
-        1-sum(sapply(ers$p,function(x){min(x,1-x)<=0.025}))/length(ers$p),
-        ks.test(ers$q,pnorm)$statistic)
-    }
-  })
-  return(data.frame(rss = this.stats[1,],h=this.stats[2,],cp95=this.stats[3,],D=this.stats[4,],model=c("BM","True model")))
-}))
-# contribution results
-contribution.df = do.call(rbind,lapply(1:dim(pars.contribution)[1],function(i){
-  asr.res = readRDS(paste0("simulation_PEonly","/bac.simulation.simulation.PEonly.",pars.contribution$id[i],".RDS"))
-  this.stats = sapply(asr.res,function(y){
-    if(is.data.frame(y$CV)){
-      ers = pseudo.Z.score(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$x,
-                           error = y$CV$var,epsilon = y$FMR$params$epsilon,laplace = FALSE)
-      c(analyzeResidualErrorByCI(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$x,
-                                     error = y$CV$var,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20),
-        analyzeResidualErrorByHeteroscedasticity(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$x,
-                                                 error = y$CV$var,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20)[1],
-        1-sum(sapply(ers$p,function(x){min(x,1-x)<=0.025}))/length(ers$p),
-        ks.test(ers$q,pnorm)$statistic)
-    }else{
-      ers = pseudo.Z.score(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$cv$x,
-                           error = y$CV$error,epsilon = y$FMR$params$epsilon,laplace = FALSE)
-      c(analyzeResidualErrorByCI(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$cv$x,
-                                     error = y$CV$error,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20),
-        analyzeResidualErrorByHeteroscedasticity(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$cv$x,
-                                                 error = y$CV$error,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20)[1],
-        1-sum(sapply(ers$p,function(x){min(x,1-x)<=0.025}))/length(ers$p),
-        ks.test(ers$q,pnorm)$statistic)
-    }
-  })
-  return(data.frame(rss=this.stats[1,],h=this.stats[2,],cp95=this.stats[3,],D=this.stats[4,],model=c("BM","True model")))
-}))
-contribution.ace.df = do.call(rbind,lapply(1:dim(pars.contribution)[1],function(i){
-  asr.res = readRDS(paste0("simulation_PEonly","/bac.simulation.simulation.PEonly.",pars.contribution$id[i],".RDS"))
+  asr.res = readRDS(paste0(epsilon.path,epsilon.name,pars.epsilon$id[i],".RDS"))
   this.stats = sapply(asr.res,function(y){
     if(is.data.frame(y$ASR)){
       ers = pseudo.Z.score(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$x,
@@ -122,9 +78,68 @@ contribution.ace.df = do.call(rbind,lapply(1:dim(pars.contribution)[1],function(
   return(data.frame(rss = this.stats[1,],h=this.stats[2,],cp95=this.stats[3,],D=this.stats[4,],model=c("BM","True model")))
 }))
 
-# Figure S2 row 1
+# parse results of simulation with pulsed evolution
+contribution.path = "Result/simulation_contribution/"
+contribution.name = "simulation.contribution"
+## hidden states
+contribution.df = do.call(rbind,lapply(1:dim(pars.contribution)[1],function(i){
+  asr.res = readRDS(paste0(contribution.path,contribution.name,pars.contribution$id[i],".RDS"))
+  this.stats = sapply(asr.res,function(y){
+    if(is.data.frame(y$CV)){
+      ers = pseudo.Z.score(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$x,
+                           error = y$CV$var,epsilon = y$FMR$params$epsilon,laplace = FALSE)
+      c(analyzeResidualErrorByCI(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$x,
+                                     error = y$CV$var,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20),
+        analyzeResidualErrorByHeteroscedasticity(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$x,
+                                                 error = y$CV$var,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20)[1],
+        1-sum(sapply(ers$p,function(x){min(x,1-x)<=0.025}))/length(ers$p),
+        ks.test(ers$q,pnorm)$statistic)
+    }else{
+      ers = pseudo.Z.score(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$cv$x,
+                           error = y$CV$error,epsilon = y$FMR$params$epsilon,laplace = FALSE)
+      c(analyzeResidualErrorByCI(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$cv$x,
+                                     error = y$CV$error,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20),
+        analyzeResidualErrorByHeteroscedasticity(trait = y$trait[y$FMR$phy$tip.label],pred = y$CV$cv$x,
+                                                 error = y$CV$error,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20)[1],
+        1-sum(sapply(ers$p,function(x){min(x,1-x)<=0.025}))/length(ers$p),
+        ks.test(ers$q,pnorm)$statistic)
+    }
+  })
+  return(data.frame(rss=this.stats[1,],h=this.stats[2,],cp95=this.stats[3,],D=this.stats[4,],model=c("BM","True model")))
+}))
+## ancestral states
+contribution.ace.df = do.call(rbind,lapply(1:dim(pars.contribution)[1],function(i){
+  asr.res = readRDS(paste0(contribution.path,contribution.name,pars.contribution$id[i],".RDS"))
+  this.stats = sapply(asr.res,function(y){
+    if(is.data.frame(y$ASR)){
+      ers = pseudo.Z.score(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$x,
+                           error = y$ASR$var,epsilon = y$FMR$params$epsilon,laplace = FALSE)
+      c(analyzeResidualErrorByCI(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$x,
+                                 error = y$ASR$var,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20),
+        analyzeResidualErrorByHeteroscedasticity(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$x,
+                                                 error = y$ASR$var,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20)[1],
+        1-sum(sapply(ers$p,function(x){min(x,1-x)<=0.025}))/length(ers$p),
+        ks.test(ers$q,pnorm)$statistic)
+    }else{
+      ers = pseudo.Z.score(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$ace$x,
+                           error = y$ASR$error,epsilon = y$FMR$params$epsilon,laplace = FALSE)
+      c(analyzeResidualErrorByCI(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$ace$x,
+                                 error = y$ASR$error,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20),
+        analyzeResidualErrorByHeteroscedasticity(trait = y$trait[-(1:Ntip(y$FMR$phy))],pred = y$ASR$ace$x,
+                                                 error = y$ASR$error,epsilon = y$FMR$params$epsilon,laplace = FALSE,bin = 20)[1],
+        1-sum(sapply(ers$p,function(x){min(x,1-x)<=0.025}))/length(ers$p),
+        ks.test(ers$q,pnorm)$statistic)
+    }
+  })
+  return(data.frame(rss = this.stats[1,],h=this.stats[2,],cp95=this.stats[3,],D=this.stats[4,],model=c("BM","True model")))
+}))
+
+# Figure S2
+## set graph parameters shared between panels
 plot.alpha=1
 dot.size = 0.5
+
+## row 1 (Time-independent variation, hidden states)
 {
   epsilon.summary = epsilon.df
   epsilon.summary$epsilon = rep(pars.epsilon$epsilon,each=2)
@@ -168,7 +183,7 @@ dot.size = 0.5
           axis.text = element_text(hjust = 1))  
 }
 
-# row 2
+## row 2 (Time independent variation, ancestral states)
 {
   epsilon.ace.summary = epsilon.ace.df
   epsilon.ace.summary$epsilon = rep(pars.epsilon$epsilon,each=2)
@@ -212,7 +227,7 @@ dot.size = 0.5
           axis.text = element_text(hjust = 1))
 }
 
-# row 3
+## row 3 (Pulsed evolution, hidden states)
 {
   contribution.summary = contribution.df
   contribution.summary$contribution = rep(pars.contribution$contribution,each=2)
@@ -255,7 +270,7 @@ dot.size = 0.5
           axis.text = element_text(hjust = 1))
 }
 
-# row 4
+## row 4 (Pulsed evolution, ancestral states)
 {
   contribution.ace.summary = contribution.ace.df
   contribution.ace.summary$contribution = rep(pars.contribution$contribution,each=2)
@@ -297,23 +312,24 @@ dot.size = 0.5
           axis.line = element_line(),
           axis.text = element_text(hjust = 1))
 }
-#
+
+## combine panels together
 combined.trend.plot = ggarrange(plotlist = list(epsilon.D.plot,epsilon.h.plot,epsilon.er.plot,
                                                 epsilon.ace.D.plot,epsilon.ace.h.plot,epsilon.ace.er.plot,
                                                 contribution.D.plot,contribution.h.plot,contribution.er.plot,
                                                 contribution.ace.D.plot,contribution.ace.h.plot,contribution.ace.er.plot),
                                 ncol = 3,nrow = 4,align = "hv",labels = "AUTO",common.legend = TRUE,legend = "bottom",label.x = c(0,0,-0.05,0,0,-0.05))
-#
+## save the graph to files
 ggsave(filename = "FigS2.pdf",device = "pdf",plot = combined.trend.plot,path = "./",
        scale = 3,width = 3.25,height = 4.5,units = "in")
 ggsave(filename = "FigS2.png",device = "png",plot = combined.trend.plot,path = "./",
        scale = 3,width = 3.25,height = 4.5,units = "in",dpi = "print",type="cairo")
-#
+# display key numbers described in the preprint
 epsilon.improve = sapply(seq(1,dim(epsilon.summary)[1],2),function(i){1-(unlist(epsilon.summary[i+1,c(1,2,4)])-1)/(unlist(epsilon.summary[i,c(1,2,4)])-1)})
 contribution.improve = sapply(seq(1,dim(contribution.summary)[1],2),function(i){1-(unlist(contribution.summary[i+1,c(1,2,4)])-1)/(unlist(contribution.summary[i,c(1,2,4)])-1)})
 epsilon.ace.improve = sapply(seq(1,dim(epsilon.ace.summary)[1],2),function(i){1-(unlist(epsilon.ace.summary[i+1,c(1,2,4)])-1)/(unlist(epsilon.ace.summary[i,c(1,2,4)])-1)})
 contribution.ace.improve = sapply(seq(1,dim(contribution.ace.summary)[1],2),function(i){1-(unlist(contribution.ace.summary[i+1,c(1,2,4)])-1)/(unlist(contribution.ace.summary[i,c(1,2,4)])-1)})
-#
-save.image("trend.summary.RData")
+## save workspace (need manual activation)
+#save.image("trend.summary.RData")
 
 
