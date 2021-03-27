026aebf04879428fddf157b5f568233c0190c04f
Yingnan Gao <52718100+gaoyingnan-academic@users.noreply.github.com>
2021-03-27 15:01:45 -0400

    Update PEpois.R

--- R/PEpois.R
+++ R/PEpois.R
@@ -1,19 +1,28 @@
-#' Probability density function of trait change under pulsed evolution
+#' @title  Probability functions of trait change under pulsed evolution with 1 compound Poisson process
 #'
-#' This function returns the probability density of having a trait change x given branch length t
+#' @description  Density function, cumulative distribution function and random generation for the trait change under Pulsed evolution with 1 compound Poisson process
 #'
 #' @param x the trait change between two nodes
-#' @param t the branch length bewteen two nodes
+#' @param q the trait change between two nodes
+#' @param n the number of random values to return
+#' @param t the branch length between two nodes
 #' @param lambda the rate at which jumps happen
 #' @param size the variance of trait change introduced per jump
 #' @param sigma the rate of BM (variance of gradual trait change per unit branch length)
 #' @param epsilon the variance of time-independent variation
 #' @param margin the total probability mass of the number of jumps not covered, defaults to 1e-6
-#' @return The numeric value of probability density
-#' @seealso [pPEpoisnorm()]
-#' @export dPEpoisnorm
+#' @param lower.tail logical, if TRUE (default), probability of trait change smaller or equal to q is returned. If false, the probability of being greater is returned.
+#' @return `dPEpoisnorm` returns the probability density
+#' @return `pPEpoisnorm` returns the cumulative distribution
+#' @return `rPEpoisnorm` returns a vector of random numbers following the specified distribution
 #' @examples
-#' dPEpoisnorm(x=0,t=1,lambda=1,size=1,sigma=1,epsilon=0,margin=1e-6)
+#' # generate some random trait changes under pulsed evolution
+#' dtrait = rPEpoisnorm(n=1e3,t=1,lambda=1,size=1,sigma=1,epsilon=0.1)
+#'
+#' # test the empirical distribution against the theoretical one
+#' ks.test(dtrait,pPEpoisnorm,t=1,lambda=1,size=1,sigma=1,epsilon=0.1)
+#' @export
+#' @rdname PEpoisnorm
 dPEpoisnorm = function(x,t,lambda,size,sigma,epsilon=0,margin=1e-6){
   En = lambda*t
   if(En>1e4){
@@ -27,23 +36,9 @@ dPEpoisnorm = function(x,t,lambda,size,sigma,epsilon=0,margin=1e-6){
   return(p)
 }
 
-#' Cumulative distribution function of trait change under pulsed evolution
-#'
-#' This function returns the cumulative probability of having a trait change q given branch length t
-#'
-#' @param q the trait change between two nodes
-#' @param t the branch length bewteen two nodes
-#' @param lambda the rate at which jumps happen
-#' @param size the variance of trait change introduced per jump
-#' @param sigma the rate of BM (variance of gradual trait change per unit branch length)
-#' @param epsilon the variance of time-independent variation
-#' @param margin the total probability mass of the number of jumps not covered, defaults to 1e-6
-#' @param lower.tail whether
-#' @return The numeric value of probability density
-#' @seealso [dPEpoisnorm()]
+
 #' @export
-#' @examples
-#' pPEpoisnorm(q=0,t=1,lambda=1,size=1,sigma=1,epsilon=0)
+#' @rdname PEpoisnorm
 pPEpoisnorm = function(q,t,lambda,size,sigma,epsilon=0,margin=1e-6,lower.tail=TRUE){
   En = lambda*t
   if(En>1e4){
@@ -58,34 +53,11 @@ pPEpoisnorm = function(q,t,lambda,size,sigma,epsilon=0,margin=1e-6,lower.tail=TR
 }
 
 #' @export
+#' @rdname PEpoisnorm
 rPEpoisnorm = function(n,t,lambda,size,sigma,epsilon=0){
   En = lambda*t
   k = rpois(n = n,lambda = En)
   sds = sqrt(k*size+sigma*t+epsilon)
   x = rnorm(n = n,mean = 0,sd = sds)
   return(x)
-}
-
-#' @export
-dMixNormal = function(x,mean,sd,probs){
-  dd = sapply(x,function(xx){
-    sum(dnorm(x = xx,mean = mean,sd = sd)*probs)
-  })
-  return(dd)
-}
-
-#' @export
-pMixNormal = function(q,mean,sd,probs){
-  pp = sapply(q,function(qq){
-    sum(pnorm(q = qq,mean = mean,sd = sd)*probs)
-  })
-  return(pp)
-}
-
-#' @export
-rMixNormal = function(n,mean,sd,probs){
-  rr = sapply(1:n,function(i){
-    sample(x=rnorm(n = length(probs),mean = mean,sd = sd),size = 1,replace = TRUE,prob = probs)
-  })
-  return(rr)
 }
