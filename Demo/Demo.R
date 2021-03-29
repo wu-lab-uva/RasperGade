610f256f49a49f3166e25873297968ab363959bf
Yingnan Gao <52718100+gaoyingnan-academic@users.noreply.github.com>
2021-03-29 14:19:58 -0400

    Update Demo.R

--- Demo/Demo.R
+++ Demo/Demo.R
@@ -4,3 +4,35 @@ install_github(repo="wu-lab-uva/RasperGade")
 
 # if you have RasperGade installed
 library(RasperGade)
+
+# read the demo tree and trait values
+tree = read.tree("demo.tree")
+trait = read.table("demo.trait.txt",sep = "\t",stringsAsFactors = FALSE)[,2]
+names(trait) = read.table("demo.trait.txt",sep = "\t",stringsAsFactors = FALSE)[,1]
+# 'fit' a BM model using PIC
+bm.rate = var(pic(x = trait[tree$tip.label],phy = tree))
+
+# fit the pulsed evolution model
+## takes several seconds to complete
+pe.model = fitPE.phy(phy = tree,x = trait[tree$tip.label],laplace = FALSE)
+
+# conducting leave-one-out cross-validation under different models
+## takes about a minute to complete
+bm.cv = do.call(rbind,lapply(1:Ntip(tree),function(i){predTraitByPIC(phy = tree,trait = trait[tree$tip.label[-i]],rate = bm.rate)}))
+pe.fmr = fullMarginalReconstructionWithPE(phy = tree,x = trait[tree$tip.label],params = pe.model$params,laplace = FALSE)
+pe.cv = crossValidationWithPE(FMR = pe.fmr,add.epsilon = FALSE,laplace = FALSE)
+
+# get the residual errors
+bm.error = trait[tree$tip.label] - bm.cv$x
+pe.error = trait[tree$tip.label] - pe.cv$cv$x
+
+# compare the error and bias
+## bias
+print(c(BM=mean(bm.error),PE=mean(pe.error)))
+## error
+print(c(BM=mean(abs(bm.error)),PE=mean(abs(pe.error))))
+
+# how normal are Z-scores?
+bm.D = analyzeResidualErrorByPPplot(trait = trait[tree$tip.label],pred = bm.cv$x,error = bm.cv$var,epsilon = 0,laplace = FALSE)
+pe.D = analyzeResidualErrorByPPplot(trait = trait[tree$tip.label],pred = pe.cv$cv$x,error = pe.cv$error,epsilon = pe.fmr$params$epsilon,laplace = FALSE)
+print(c(BM=bm.D,PE=pe.D))
