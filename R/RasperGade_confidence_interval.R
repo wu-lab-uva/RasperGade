2f9d1735e1e89a43c7412c28481883eaa2842b84
Yingnan Gao <yg5ap@virginia.edu>
2022-01-15 12:32:16 -0500

    Update RasperGade_confidence_interval.R
    add tolerance option

--- R/RasperGade_confidence_interval.R
+++ R/RasperGade_confidence_interval.R
@@ -3,19 +3,20 @@
 #' @description  Calculate the confidence interval from the calculated posterior probability
 #'
 #' @param error a data frame listing the mean, variance and weight of normal distributions
-#' @param alpha the desired confidence level
+#' @param alpha the desired confidence level;default to 0.05
+#' @param tol the precision of calculated CI that is passed to `uniroot` with the same default
 #' @export
 #' @rdname calculate_CI
-calculate_CI_from_error_distribution = function(error,alpha=0.05){
+calculate_CI_from_error_distribution = function(error,alpha=0.05,tol=.Machine$double.eps^0.25){
   lower.bound = qnorm(p = alpha/2,mean = error$x,sd = sqrt(error$var),lower.tail = TRUE)
   upper.bound = qnorm(p = alpha/2,mean = error$x,sd = sqrt(error$var),lower.tail = FALSE)
   eqa = function(q,lower.tail=TRUE){
     pMixNormal(q = q,lower.tail = lower.tail,
                mean = error$x,sd = sqrt(error$var),probs = error$probs)-alpha/2
   }
-  lower.root = uniroot(f = eqa,lower.tail=TRUE,
+  lower.root = uniroot(f = eqa,lower.tail=TRUE,tol = tol,
                        lower = min(lower.bound)-0.1,upper = max(lower.bound)+0.1)$root
-  upper.root = uniroot(f = eqa,lower.tail=FALSE,
+  upper.root = uniroot(f = eqa,lower.tail=FALSE,tol = tol,
                        lower = min(upper.bound)-0.1,upper = max(upper.bound)+0.1)$root
   return(c(lower=lower.root,upper=upper.root))
 }
